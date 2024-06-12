1c6636c570312b2708d30c7cb8727cda5cd9af51
GitHub Action <action@github.com>
2024-06-12 15:01:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/blarc/sopsintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/blarc/sopsintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.blarc.sopsintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.blarc.sopsintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
